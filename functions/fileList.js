416f4c42c1bd1dec93c3e74f5af73013cc8614af
Phil Alsford <palsford@linz.govt.nz>
2021-10-26 09:44:51 +1300

    Consume backend API

--- functions/fileList.js
+++ functions/fileList.js
@@ -1,16 +1,10 @@
 export async function getMP3FileList() {
-  const fetchResult = await fetch(
-    "https://api.github.com/repos/philals/nextjs-the-sound-system/git/trees/main?recursive=1"
-  );
+  const fetchResult = await fetch("/api/files");
   const json = await fetchResult.json();
 
-  const files = json.tree
-    .filter((file) => file.path.includes("public/mp3") && file.type === "blob")
-    .map((file) => file.path.replace("public", ""));
-
-  return files;
+  return json;
 }
 
-export async function fetchFilesFromGitHub() {
-    return await getMP3FileList()
-  }
+export async function fetchFiles() {
+  return await getMP3FileList();
+}
